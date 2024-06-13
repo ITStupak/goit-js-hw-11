f679498b05e04bc7e93f293df2c9c620c961d1be
ITStupak <itstupak@gmail.com>
2024-06-13 00:58:33 +0000

    Deploying to gh-pages from @ ITStupak/goit-js-hw-11@41ccf3b567e8f96f3205449ea05e5260e3e56984 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,30 +1,29 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const s of e.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&a(s)}).observe(document,{childList:!0,subtree:!0});function p(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function a(t){if(t.ep)return;t.ep=!0;const e=p(t);fetch(t.href,e)}})();const r=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Haulage Freight"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",description:"Aerial Beach View"},{preview:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",original:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",description:"Flower Blooms"},{preview:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",original:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",description:"Alpine Mountains"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",description:"Mountain Lake Sailing"},{preview:"https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",description:"Alpine Spring Meadows"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",description:"Nature Landscape"},{preview:"https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",description:"Lighthouse Coast Sea"}],c=document.querySelector(".gallery");function n({preview:i,original:o,description:p}){return`<li class="gallery-item">
-        <a class="gallery-link" href="${o}">
+import"./assets/vendor-b48cf979.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&c(a)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();function i(r){const s=`https://pixabay.com/api/?${new URLSearchParams({key:"44328072-f56b95eb73841ff5e619bc345",q:r,image_type:"photo",orientation:"horizontal",per_page:"100",safesearch:!1})}`;return fetch(s).then(c=>c.json())}function l({largeImageURL:r,previewURL:o,id:s,comments:c,downloads:e,likes:t,views:a}){return`<li class="gallery-item">
+        <a class="gallery-link" href="${r}">
           <img
             class="gallery-image"
-            src="${i}"
-            data-source="${o}"
-            alt="${p}"
+            src="${r}"
+            data-source="${r}"
+            alt="${s}"
           />
         </a>
         <div class="gallery-text">
           <div class="text-property">
             <p class="text-dscr">Likes</p>
-            <p class="text-count">1813</p>
+            <p class="text-count">${t}</p>
           </div>
           <div class="text-property">
             <p class="text-dscr">Views</p>
-            <p class="text-count">900290</p>
+            <p class="text-count">${a}</p>
           </div>
           <div class="text-property">
             <p class="text-dscr">Comments</p>
-            <p class="text-count">229</p>
+            <p class="text-count">${c}</p>
           </div>
           <div class="text-property">
             <p class="text-dscr">Downloads</p>
-            <p class="text-count">610937</p>
+            <p class="text-count">${e}</p>
           </div>
         </div>
-    </li>`}function l(i){return i.map(n).join(`
-`)}const d=l(r);c.innerHTML=d;
+    </li>`}function u(r){return r.hits.map(l).join("")}const n=document.querySelector(".form"),p=document.querySelector(".gallery"),d=document.querySelector(".loader");n.addEventListener("submit",f);function f(r){r.preventDefault();const o=r.target.elements.request.value.trim();i(o).then(s=>{if(d.classList.toggle("is-visible"),s.total!==0){const c=u(s);p.insertAdjacentHTML("beforeend",c)}else alert("Sorry, there are no images matching your search query. Please try again!")}).catch(s=>console.log(s)),n.reset()}
 //# sourceMappingURL=commonHelpers.js.map
